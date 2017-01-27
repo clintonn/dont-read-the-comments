18cf064b4b2daa967ea983c13b95bb25029ef608
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-01-27 12:46:47 -0500

    Added live text updating methods

--- javascript/models/round.js
+++ javascript/models/round.js
@@ -1,12 +1,11 @@
 class Round{
 
   constructor(){
-    this.$text_to_type = $("#text-to-type")
+    this.$textToType = $("#text-to-type")
     this.selectedText = this.textSelection()
     this.displayText = this.selectedText.displayer
     this.inputText = this.selectedText.inputText
-    this.$text_to_type.append(this.selectedText.text)
-    this.$roundText = $('#full-text')
+    this.$textToType.append(this.selectedText.text)
     this.$inputArea = $('#typing-area')
     this.currentIndex = 0
     this.setKeyCheck()
@@ -14,6 +13,7 @@ class Round{
 
   setKeyCheck() {
     this.$inputArea.keypress(this.inputComparer.bind(this))
+    this.$inputArea.keyup(this.updateText.bind(this))
   }
 
   textSelection(){
@@ -22,27 +22,34 @@ class Round{
       new BadComment("Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."),
       new BadComment("Nullam id dolor id nibh ultricies vehicula ut id elit."),
       new BadComment("Donec sed odio dui.")
-  ]
+    ]
     let randomSample = Math.floor(Math.random() * texts.length)
     return texts[randomSample]
   }
 
-   inputComparer(event, updateIndex){
-     this.updateCurrentIndex()
-     if (event.key === this.inputText[this.currentIndex]) {
-      console.log("Correct!")
+  inputComparer(event){
+    this.updateCurrentIndex()
+    if (event.key === this.inputText[this.currentIndex]) {
       this.displayText[this.currentIndex] = `<span class="correct">${this.inputText[this.currentIndex]}</span>`
-      this.$roundText.html(this.displayText.join(""))
+      this.$textToType.html(this.displayText.join(""))
     } else {
-      debugger
       console.log("Alternatively correct!")
       this.displayText[this.currentIndex] = `<span class="incorrect">${this.inputText[this.currentIndex]}</span>`
-      this.$roundText.html(this.displayText.join(""))
+      this.$textToType.html(this.displayText.join(""))
     }
   }
 
   updateCurrentIndex() {
     this.currentIndex = this.$inputArea.val().length
+  }
+
+  updateText(event) {
+    this.updateCurrentIndex()
+    let newDisplay = this.displayText.slice(0, this.currentIndex)
+    debugger
+    newDisplay = newDisplay.concat(this.inputText.slice(this.currentIndex))
+    this.displayText = newDisplay
+    this.$textToType.html(newDisplay.join(""))
   }
 
   // setCurrentIndex() {
